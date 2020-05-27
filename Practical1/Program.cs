3e9e7bcfa4f2e67b6adad90eba367824f54abb4b
Niti.Modi <17comp.niti.modi@gmail.com>
2020-05-27 11:47:44 +0530

    updated

---
+++ Practical1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //call pattern1
+            pattern1();
+
+            //call pattern2
+            pattern2();
+
+            //call pattern3
+            pattern3();
+
+            Console.Read();
+
+        }
+
+        static void pattern1()
+        {
+            Console.WriteLine("pattern1");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+
+        static void pattern2()
+        {
+            Console.WriteLine("pattern2");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+
+            }
+        }
+
+        static void pattern3()
+        {
+            Console.WriteLine("pattern3");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+
+            }
+        }
+    }
+
+
+}
---
+++ practical2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Enter your name");
+                name = Console.ReadLine();
+
+            }
+            while (!Regex.IsMatch(name, "^[a-zA-Z]+$"));
+
+
+            Console.WriteLine("Which country do you belong to?");
+            country = Console.ReadLine();
+
+            Console.WriteLine("Hi i am , " + name + " from " + country);
+
+            Console.Read();
+
+        }
+    }
+}
